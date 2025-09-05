90d7bb984e512f9bea22576c9812e07b92a14c95
Shadow <hi@shadowing.dev>
2025-09-05 08:51:00 -0500

    center the header on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
 	return (
 		<html lang="en" className={inter.className}>
 			<body className="min-h-screen flex flex-col items-center">
-				<header className="fixed top-0 left-0 right-0 z-50 w-full bg-background/80 backdrop-blur-sm border-b border-border px-5 py-1 flex flex-row justify-between items-center">
+				<header className="fixed top-0 left-0 right-0 z-50 w-full bg-background/80 backdrop-blur-sm border-b border-border px-5 py-1 flex flex-col md:flex-row justify-center md:justify-between items-center gap-2 md:gap-0">
 					<div className="flex items-center">
 						<Image
 							src={env.LOGO_URL}
